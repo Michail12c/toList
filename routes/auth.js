26c62c76cee9f35c49708d78a7e9f9ec256a88e0
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-06-04 17:17:21 +0300

    change status

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ async(req, res) => {
       {expiresIn: '1h'}
     )
     
-    return res.status(200).json({token, userId: user.id})
+     res.status(201).json({token, userId: user.id})
   }catch(e){
     res.status(500).json({message:"Error..."})
   }
