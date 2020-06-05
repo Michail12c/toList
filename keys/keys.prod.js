a2f4eebe4572caca8c9e32a9c386431bd32d0785
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-06-05 14:33:00 +0300

    new commit

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
-  SESSION_SECRET: process.env.jwtSecret,
+  SESSION_SECRET: process.env.SESSION_SECRET,
   BASE_URL: 'https://agile-dawn-95357.herokuapp.com/'
 }
